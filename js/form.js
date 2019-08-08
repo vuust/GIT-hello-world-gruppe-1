f9bb2b117e6536d19027df994158c6ee512463a1
unknown <kasperfryd@gmail.com>
2019-08-08 09:38:14 +0200

    Finished work on Forms - updated style and form in about

--- js/form.js
+++ js/form.js
@@ -1,69 +1,76 @@
 
-var x; 
-var y; 
-var h; 
-var f;
-var z; 
+let namefield; 
+let lastnamefield; 
+let numberfield; 
+let commentfield;
+let emailfield; 
+
+namefield = document.getElementById("namefield");
+lastnamefield = document.getElementById("lastnamefield");
+numberfield = document.getElementById("numberfield");
+commentfield = document.getElementById("commentfield");
+emailfield = document.getElementById("emailfield");
+
 
 function validate() {
-    x = document.getElementById("namefield").value;
-    y = document.getElementById("lastnamefield").value;
-    h = document.getElementById("numberfield").value;
-    f = document.getElementById("commentfield").value;
-    z = document.getElementById("emailfield").value;
-
-    var radiocheck = document.getElementById("accept").checked;
-    var infostring; 
+    let radiocheck = document.getElementById("accept").checked;
+    let namevalue = namefield.value;
+    let lastnamevalue = lastnamefield.value;
+    let numbervalue = numberfield.value;
+    let commentvalue = commentfield.value;
+    let emailvalue = emailfield.value;
     
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
+    if (namevalue == "") {
+        alert("Name must be filled out");
+        namefield.focus();
+        return false;
     }
     
-    if (y == "") {
+    if (lastnamevalue == "") {
         alert("Last name must be filled out");
+        lastnamefield.focus()
         return false;
     }
-      
-    if (h == "") {
+    
+    if (numbervalue == "") {
         alert("Number must be filled out");
+        numberfield.focus()
         return false;
     }
-
-    if (z == "") {
-        alert("Email must be filled out")
-        return false;
-    }
-
-    if (h.length < 8){
+    if (numbervalue.length < 8){
+        numbervalue.focus()
         alert("Number must be atleast 8 characters")
         return false;
     }
-
+    
+    if (emailvalue == "") {
+        emailfield.focus()
+        alert("Email must be filled out")
+        return false;
+    }
+    
+    if (isEmailKey() == false){
+        alert("You need to enter a valid email")
+        return false;
+    }
+    
     if (!radiocheck){
         alert("You need to accept");
         return false;
     }
-
-    if (radiocheck == true){
+    
+    else {
+        alert("Name: " + namevalue + "\n" + "Last name: " + lastnamevalue + "\n" + "Phone: " + numbervalue + "\n" + "Email: " + emailvalue + "\n" + "Comment: " + commentvalue + "\n" + "Your information has been submitted");
+        location.href="about.html";
     }
     
-    if (isEmailKey() == true){
-    location.href = "landingpage.html";
-        alert("Name: " + x + "\n" + "Last name: " + y + "\n" + "Phone: " + h + "\n" + "Email: " + z + "\n" + "Comment: " + f + "\n" + "Your information has been submitted");
-    }
 }
-
-function onPageLoad() {
-    document.getElementById("name").innerHTML = infostring-name;
-}
-
 function isNumberKey(evt)
 {
-	var charCode = (evt.which) ? evt.which : event.keyCode
+    var charCode = (evt.which) ? evt.which : event.keyCode
     if (charCode > 31 && (charCode < 48 || charCode > 57))
-		return false;
-
+    return false;
+    
 	return true;
 }
 
@@ -71,17 +78,17 @@ function isLetterKey(evt)
 {
     var charCode2 = (evt.which) ? evt.which : event.keyCode
     if (charCode2 < 60)
-        return false;
-
+    return false;
+    
 	return true;
 }
 
 function isEmailKey() 
 {
- if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailfield.value))
-  {
-    return (true)
-  }
-    alert("You have entered an invalid email address!")
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailfield.value))
+    {
+        return (true)
+    }
+    else
     return (false)
 }
